e873ed48ce6c619af91554a2f1cc11a8548fc1ad
Nick Barry <niko.barry@gmail.com>
2016-10-06 16:03:10 -0700

    chore: Add test-watching capabilities
    Mocha can now watch my code and re-run the tests every time the code changes.

--- test/test.js
+++ test/test.js
@@ -19,3 +19,22 @@ describe('Name capitalization', () => {
 
 });
 */
+
+/*
+More test cases / edge cases / etc.
+Should always return a string of the same length
+
+ McSweeney
+ MacArthur
+ diGiorno
+ d'Giorno
+ di Giorno
+ D'Giorno
+ DiGiorno
+ de los Riesgo
+ Harper-Smith
+ III (not Iii)
+ All-caps org names, e.g. RESULTS
+ look up article on name capitalization exceptions
+ ballot items as well
+ */
